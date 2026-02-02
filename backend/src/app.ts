01fd573679e1a82de9593c593def73335b769999
Alisson Morais <alissonmorais.br@gmail.com>
2026-02-02 03:05:00 -0300

    fix: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,26 @@ import Fastify from 'fastify'
 import { registerRoutes } from './routes/index.js'
 import { jwtMiddleware } from './infra/jwt.middleware.js'
 import { analysisRoutes } from './modules/analysis/analysis.routes.js'
+import cors from '@fastify/cors'
 
-export function buildApp() {
+export async function buildApp() {
   const app = Fastify({
     logger: true
   })
+
+//   await app.register(cors, {
+//   origin: [
+//     'https://apps-cronus-flow.ptiotg.easypanel.host',
+//     /^chrome-extension:\/\// // 🔴 ESSENCIAL
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// });
+
+await app.register(cors, {
+  origin: true
+});
+
   app.addHook('onRequest', jwtMiddleware)
   registerRoutes(app)
   app.register(analysisRoutes, { prefix: '/v1' })
