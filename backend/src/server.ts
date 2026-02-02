120a49e68fb7f1c6c4441d76714c2fe8061d0a6d
Alisson Morais <alissonmorais.br@gmail.com>
2026-02-02 03:18:36 -0300

    fix: add cors 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,6 @@ import { buildApp } from './app.js'
 import { env } from './config/env.js'
 
 await connectMongo()
-const app = buildApp()
+const app = await buildApp()
 
 app.listen({ port: env.PORT, host: '0.0.0.0' })
