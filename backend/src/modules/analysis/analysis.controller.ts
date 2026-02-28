754958006261a109eb2539b8978936f36cdee0a0
Alisson Morais <alissonmorais.br@gmail.com>
2026-02-28 16:34:18 -0300

    feat: get feedback count

--- backend/src/modules/analysis/analysis.controller.ts
+++ backend/src/modules/analysis/analysis.controller.ts
@@ -1,3 +1,4 @@
+import { Analysis } from './analysis.model.js'
 import { listUserAnalyses } from './analysis.service.js'
 
 export async function listMyAnalysesController(request: any) {
@@ -6,4 +7,15 @@ export async function listMyAnalysesController(request: any) {
   const analyses = await listUserAnalyses(userId)
 
   return analyses
+}
+
+export async function getFeedbackCount(request: any, reply: any) {
+  const userId = request.user.userId
+
+  const count = await Analysis.countDocuments({
+    userId,
+    converted: { $ne: null }
+  })
+
+  return reply.send({ count })
 }
--- backend/src/modules/analysis/analysis.routes.ts
+++ backend/src/modules/analysis/analysis.routes.ts
@@ -1,8 +1,9 @@
-import { listMyAnalysesController } from './analysis.controller.js'
+import { getFeedbackCount, listMyAnalysesController } from './analysis.controller.js'
 import { analyzeController } from '../analyze/analyze.controller.js'
 import { FastifyInstance } from 'fastify'
 
 export async function analysisRoutes(app: FastifyInstance) {
   app.get('/my-analyses', listMyAnalysesController)
   app.post('/analyze', analyzeController)
+  app.get('/feedback/count', getFeedbackCount)
 }
